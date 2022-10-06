d47e94f0cdd031b93b947f3e3ba86d06851e67f3
Oberwalder Sven <sven.oberwalder@icloud.com>
2022-10-06 08:50:38 +0200

    DONE DONE DONE DONE

--- Lab1/script.js
+++ Lab1/script.js
@@ -49,3 +49,26 @@ function addItem(item) {
 
     set_amount()
 }
+
+//Drag and drop
+
+
+function dragStart(event) {
+    event.dataTransfer.setData("img", event.target.id);
+}
+
+/* Function of allow drop content */
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+/* Function of drop content */
+function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("img");
+
+    addItem(parseInt(data))
+
+    console.log(data)
+
+}
