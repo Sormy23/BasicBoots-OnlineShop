6508ada39556753cfd1255d34efa919c487833b0
Oberwalder Sven <sven.oberwalder@icloud.com>
2022-12-13 08:55:00 +0100

    I'm going to commit suicide and push myself off a cliff

--- Lab2/src/main/java/at/ac/htlwrn/dao/ProductOrderDao.java
+++ Lab2/src/main/java/at/ac/htlwrn/dao/ProductOrderDao.java
@@ -1,13 +1,13 @@
 package at.ac.htlwrn.dao;
 
-import at.ac.htlwrn.model.Product;
+import at.ac.htlwrn.model.ProductOrder;
+import at.ac.htlwrn.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ProductDao extends CrudRepository<Product, Long> {
-    
+public interface ProductOrderDao extends CrudRepository<ProductOrder, Long> {
 
 }
