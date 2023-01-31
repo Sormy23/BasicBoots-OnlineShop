33605879a11c6391e41f9d9854ea6ef1e424cdb7
Oberwalder Sven <sven.oberwalder@icloud.com>
2023-01-31 08:54:44 +0100

    I'm going to commit suicide and push myself off a cliff

--- Lab2/src/main/java/at/ac/htlwrn/dto/PurchaseOrderDto.java
+++ Lab2/src/main/java/at/ac/htlwrn/dto/PurchaseOrderDto.java
@@ -1,4 +1,28 @@
 package at.ac.htlwrn.dto;
 
+import javax.persistence.Column;
+import java.sql.Timestamp;
+
 public class PurchaseOrderDto {
+    private Long id;
+
+    private String anrede;
+
+    private String vorname;
+
+    private String name;
+
+    private String street;
+
+    private String zipCode;
+
+    private String city;
+
+    private Timestamp date;
+
+    private float price;
+
+    private Timestamp finished;
+
+    private Timestamp canceled;
 }
