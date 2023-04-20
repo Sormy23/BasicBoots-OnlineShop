0d86fc98f146f41c446da0fbfdb59a4918d98e92
Oberwalder Sven <sven.oberwalder@icloud.com>
2023-04-20 12:21:48 +0200

    I'm going to commit suicide and push myself off a cliff

--- Lab2/src/test/java/at/ac/htlwrn/service/impl/PurchaseOrderServiceImplTest.java
+++ Lab2/src/test/java/at/ac/htlwrn/service/impl/PurchaseOrderServiceImplTest.java
@@ -1,4 +1,35 @@
 package at.ac.htlwrn.service.impl;
 
+import at.ac.htlwrn.dto.ProductDto;
+import at.ac.htlwrn.dto.PurchaseOrderDto;
+import at.ac.htlwrn.model.Product;
+import at.ac.htlwrn.model.PurchaseOrder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Optional;
+
+@AutoConfigureTestDatabase
+@SpringBootTest
 public class PurchaseOrderServiceImplTest {
+    @Autowired
+    private PurchaseOrderServiceImpl purchaseOrderService;
+
+    private PurchaseOrder createOrder(String anrede, String name, String street, String zipCode, String city, Date date) {
+        PurchaseOrderDto order = new PurchaseOrderDto();
+        order.setAnrede(anrede);
+        order.setName(name);
+        order.setStreet(street);
+        order.setZipCode(zipCode);
+        order.setCity(city);
+        order.setDate(date);
+
+        return purchaseOrderService.save(order);
+    }
+
 }
