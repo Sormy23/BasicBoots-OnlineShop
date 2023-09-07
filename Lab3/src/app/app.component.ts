b98ef5926cd0587f334033c17a98037c15c5fe37
Oberwalder Sven <sven.oberwalder@icloud.com>
2023-09-07 11:25:54 +0200

    added shop, basket and imprint and correct animation of navbar

--- Lab3/src/app/app.component.ts
+++ Lab3/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lab3';
+
+  getURL() {
+    return this.router.url;
+  }
+
+  constructor(private router: Router) {
+  }
+
+  protected readonly console = console;
 }
--- Lab3/src/app/app.module.ts
+++ Lab3/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ShopComponent } from './shop/shop.component';
+import { BasketComponent } from './basket/basket.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomepageComponent,
+    ShopComponent,
+    BasketComponent,
+    ImprintComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
