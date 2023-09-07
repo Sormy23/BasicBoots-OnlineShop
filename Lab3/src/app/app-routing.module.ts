61d9a18481fac97573116e3a3897487c30208555
Oberwalder Sven <sven.oberwalder@icloud.com>
2023-09-07 11:47:00 +0200

    added shop, basket and imprint and correct animation of navbar

---
+++ Lab3/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomepageComponent } from './homepage/homepage.component';
+import { ShopComponent } from './shop/shop.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { BasketComponent } from './basket/basket.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'imprint', component: NotFoundComponent },
+  { path: 'basket', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
