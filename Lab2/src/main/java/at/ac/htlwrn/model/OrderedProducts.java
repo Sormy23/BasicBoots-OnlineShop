c36f0a4ea623c5f5afb60dedf63c9910a3aed718
Oberwalder Sven <sven.oberwalder@icloud.com>
2023-09-28 12:23:03 +0200

    FIXED IT

--- Lab2/src/main/java/at/ac/htlwrn/model/OrderedProducts.java
+++ Lab2/src/main/java/at/ac/htlwrn/model/OrderedProducts.java
@@ -1,5 +1,7 @@
 package at.ac.htlwrn.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 
@@ -17,6 +19,7 @@ public class OrderedProducts {
 
     @ManyToOne
     @JoinColumn(name = "purchaseOrder_id")
+    @JsonIgnore
     private PurchaseOrder purchaseOrder;
 
     private int quantity;
